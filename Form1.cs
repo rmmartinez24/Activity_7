d16a969bb8f1a3ede314391eb3ece6b9fe3f0532
rmmartinez24 <104732814+rmmartinez24@users.noreply.github.com>
2022-05-08 15:16:27 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double terms;
+            double pi = 4;
+            double topNum = 4;
+            double bottomNum = 3;
+
+            terms = Convert.ToInt32(textBox1.Text);
+
+            for (int i = 0; i <= terms; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    pi -= topNum / bottomNum;
+                }
+
+                else
+                {
+                    pi += topNum / bottomNum;
+                }
+
+                bottomNum += 2;
+            }
+
+            label2.Text = "Value of pi after " + terms + " terms";
+            label3.Text = "= " + pi;
+        }
+    }
+}
